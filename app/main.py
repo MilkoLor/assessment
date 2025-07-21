2be9d069437f6fe333b5a96a95e011755f4356be
MilkoLor <m.lorinkov2301@gmail.com>
2025-07-21 10:33:58 -0400

    Update main.py
    fixing CORS issue

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ app = FastAPI(title="Ad Insights Explorer Lite")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","http://127.0.0.1:8000"],
+    allow_origins=["http://localhost:5173","http://127.0.0.1:8000", "https://ad-insights-explorer.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
